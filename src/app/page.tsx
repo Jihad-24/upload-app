2adea32b7f26ca07f0ac7e37d4ecc322fbe419bf
Jihad-24 <mdjihat0021@gmail.com>
2025-10-23 19:48:54 +0600

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-zinc-50">
+      <h1 className="text-3xl font-semibold text-gray-900">Welcome to my site!</h1>
+    </div>
+  );
+}
